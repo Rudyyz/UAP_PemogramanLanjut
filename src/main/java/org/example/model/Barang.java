7b2854a9a907ceac6144c6e72bdf7a9dc0e203dc
Rudyyz <mrudimustakim@gmail.com>
2025-12-22 15:25:12 +0700

    update

--- src/main/java/org/example/model/Barang.java
+++ src/main/java/org/example/model/Barang.java
@@ -9,7 +9,6 @@ public class Barang {
     private double harga;
     private LocalDate tanggalMasuk;
 
-
     public Barang(String kode, String nama, int stok, double harga, LocalDate tanggalMasuk) {
         this.kode = kode;
         this.nama = nama;
@@ -17,19 +16,44 @@ public class Barang {
         this.harga = harga;
         this.tanggalMasuk = tanggalMasuk;
     }
+
     public String getKode() {
         return kode;
     }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
     public String getNama() {
         return nama;
     }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
     public int getStok() {
         return stok;
     }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
     public double getHarga() {
         return harga;
     }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
     public LocalDate getTanggalMasuk() {
         return tanggalMasuk;
+    }
+
+    public void setTanggalMasuk(LocalDate tanggalMasuk) {
+        this.tanggalMasuk = tanggalMasuk;
     }
 }
